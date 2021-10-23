340ceb7acb639d8f5ad338eb7c5745305ac6c751
Amador Cuenca <sphi02ac@gmail.com>
2021-10-23 09:09:04 -0300

    feat: Prototype the Team page

--- src/main/java/com/example/ipldashboard/controller/TeamController.java
+++ src/main/java/com/example/ipldashboard/controller/TeamController.java
@@ -4,10 +4,12 @@ import com.example.ipldashboard.model.Team;
 import com.example.ipldashboard.repository.MatchRepository;
 import com.example.ipldashboard.repository.TeamRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 public class TeamController {
