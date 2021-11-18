45d6e7b02a7812877a5e6f0a1f4f2b5959abdd68
Amador Cuenca <sphi02ac@gmail.com>
2021-11-18 16:17:54 -0300

    Add Match Page selector and Home Page

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -1,5 +1,4 @@
 import {React, useEffect, useState} from 'react';
-import MatchSmallCard from "../components/MatchSmallCard";
 import MatchDetailCard from "../components/MatchDetailCard";
 import {useParams} from "react-router-dom";
 
